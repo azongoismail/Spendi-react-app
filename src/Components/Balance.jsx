e5bd3e2f3b5e8b549583362901952ce7ef4136ba
Azongo Ismail <ismailibnazongo@gmail.com>
2025-06-06 00:15:29 +0000

    style the balance component

--- src/Components/Balance.jsx
+++ src/Components/Balance.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Balance = ({balance}) => {
   return (
-    <div>
-      <p>Balance: ${balance}</p>
+    <div className='bg-[#2BA3EC] w-[50vw] h-[50vh] my-4 p-4 flex items-start justify-center'>
+      <p className='text-2xl '>Balance: <span className='bg-[#EBFCFF] py-2 px-4 text-2xl justify-start w-[50vw]'>${balance}</span> </p>
     </div>
   )
 }
